bd6ea6b20a74ef6a53891fa99ac0ab9a68c74e90
«rivitch» <rivitch@rambler.ru>
2023-09-08 18:58:49 +0400

    123

--- Lec3.py
+++ Lec3.py
@@ -284,4 +284,12 @@
 # # print(type("text",))
 # # print(type(g_tuple))
 
-
+# словарь
+a = {'one': 42, 'two': 3.14, 'ten': 'Hello world!'}
+b = dict(one=42, two=3.14, ten='Hello world!')
+c = dict([('one', 42), ('two', 3.14), ('ten', 'Hello world!')])
+print(a == b == c)
+# добавление
+my_dict = {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+my_dict['ten'] = 10
+print(my_dict)
