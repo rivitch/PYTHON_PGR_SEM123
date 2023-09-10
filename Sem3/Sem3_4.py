6f8ee3949c2c9d7aa28e796997017392f566da26
«rivitch» <rivitch@rambler.ru>
2023-09-10 17:00:25 +0400

    begin 3-5

---
+++ Sem3/Sem3_4.py
@@ -0,0 +1,26 @@
+# Задание №4
+# ✔ Создайте вручную список с повторяющимися элементами.
+# ✔ Удалите из него все элементы, которые встречаются дважды.
+
+# my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
+# out = []
+# print(my_list)
+# for i in my_list:
+#     # print(i, my_list.count(i))
+#     if my_list.count(i) != 2:
+#         # print(i, my_list)
+#         out.append(i)
+#         # print(out)
+# print(out)
+
+# оставляем тех кто 2 раза
+# my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
+# out = []
+# print(my_list)
+# for i in my_list:
+#     if my_list.count(i) == 2:
+#         out.append(i)
+# print(out)
+
+
+        
