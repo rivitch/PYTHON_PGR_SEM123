d2ef7767369dde40b0baceb6027cfe9852feb6b5
«rivitch» <rivitch@rambler.ru>
2023-09-10 22:16:45 +0400

    3-6c

--- Sem3/Sem3_6.py
+++ Sem3/Sem3_6.py
@@ -8,17 +8,43 @@ _inp = "Cтроку вводит пользователь текста"
 print(_inp)
 list_1 = _inp.split()
 len_max = max(list_1, key=len)
-print(len_max)
-list_1.sort()
+print(len_max, "/",list_1)
+count = 0
+
+for i in list_1:
+    count += 1
+    print(i, type(i))
+    print(len_max, type(len_max))
+    if i == len_max:
+        print(f"{count} {i}") # {i + 1}," ",
+    else:
+        print(f"{count}{i}")  # {len[i]}
+#print(f"{i}{list_1[i] = :> key}")  
+
+# # max = int(len_max)
+# print(type(len_max), len_max)
+# #x = int(len(len_max))
+# # print(type(x))
+# # print(x)
+# list_1.sort()
 # print(list_1)
 # print(len(list_1))
-# if i == len_max:
-#     print(len_max)
-for i in range(len(list_1)):
-    # print(i + 1, list_1[i])
-    if i == len_max:
-        print(f" {i + 1} {len_max}")
-    # else:
-    #     print(f"{i}")  # {len[i]}
-    print(f"{i}{list_1[i]:>len_max}")  # {len[i]}
-    pass
+# for i in list_1:
+#     # print(i + 1, list_1[i])
+#     print(i, type(i))
+#     print(len_max, type(len_max))
+#     if i == len_max:
+#         print(f"{i + 1} {len_max}")
+# # if i == len_max:
+# #     print(len_max)
+# # for i in range(len(list_1)):
+# #     # print(i + 1, list_1[i])
+# #     print(i, type(i))
+# #     print(len_max, type(len_max))
+# #     if i == len_max:
+# #         print(f"{i + 1}," ",{x}")
+#     # else:
+#     #     print(f"{i}")  # {len[i]}
+# #print(f"{i}{list_1[i] = :> key}")   
+#     #print(f"{i}{list_1[i] = :> key}")  # {len[i]}
+#     #pass
