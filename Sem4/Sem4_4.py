0713016c8a27df3668fdf58bbe86ac81ee671780
«rivitch» <rivitch@rambler.ru>
2023-09-12 02:01:28 +0400

    4_8

--- Sem4/Sem4_4.py
+++ Sem4/Sem4_4.py
@@ -8,5 +8,20 @@ my_list = []
 for i in range(10):
     my_list.append(randint(0, 10))
     #print(my_list[i])
-#print(my_list)
+print(my_list)
+# def F4_4(x):
+
+#     pass
+# print(F4_4(my_list))
+
+# семинарное (сортировка пузырьком)
+def my_sort(some_lst:list):
+    for i in range(len(some_lst)):
+        for j in range(len(some_lst) -i -1):
+            if some_lst[j] > some_lst[j + 1]:
+                some_lst[j], some_lst[j + 1] = some_lst[j+ 1], some_lst[j]
+    return some_lst
+my_sort(my_list)
+print(my_list)   
+
 
