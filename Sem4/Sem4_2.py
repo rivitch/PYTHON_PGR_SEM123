7342e3d736165090505a5047011b4f4247c787aa
«rivitch» <rivitch@rambler.ru>
2023-09-11 18:08:10 +0400

    4_2c

--- Sem4/Sem4_2.py
+++ Sem4/Sem4_2.py
@@ -3,9 +3,53 @@
 # ✔ Сформируйте список с уникальными кодами Unicode каждого 
 # символа введённой строки отсортированный по убыванию.
 
-_str1 = "Напишите функцию"  #, которая принимает строку текста."
+_str1 = "Напишите функцию, которая принимает строку текста."
 def F4_2(data: str):
-    my_list = sorted(data,reverse=True)
-    res = 
-    return my_list
-print(F4_2(_str1))
+    #list_1 = data.split()
+    my_list = sorted(data)
+    set_list = sorted(list(set(my_list)),reverse=True)
+    #len_max =len(max(list_1, key=len))
+    #list_1.sort()
+    for spam, eggs in enumerate(set_list, 0):
+        print(f"{ord(eggs)} - {eggs}")
+F4_2(_str1)
+
+
+# my_list = sorted(_str1)
+# #set_list = {}
+# # set_list = set(my_list)
+# # set_list=sorted(list(set(my_list)))
+# #print(type(my_list),my_list,"\n",type(set_list),set_list)
+# set_list = sorted(list(set(my_list)),reverse=True)
+# # print(type(set_list))
+# # print(type(my_list),my_list,"\n",type(set_list),set_list,"\n")
+# for spam, eggs in enumerate(set_list, 0):
+#     print(f"{ord(eggs)} - {eggs}")
+
+
+
+# _str1 = "Напишите функцию, которая принимает строку текста."
+# my_list = sorted(_str1)
+# #set_list = {}
+# # set_list = set(my_list)
+# # set_list=sorted(list(set(my_list)))
+# #print(type(my_list),my_list,"\n",type(set_list),set_list)
+# set_list = sorted(list(set(my_list)),reverse=True)
+# # print(type(set_list))
+# # print(type(my_list),my_list,"\n",type(set_list),set_list,"\n")
+# for spam, eggs in enumerate(set_list, 0):
+#     print(f"{ord(eggs)} - {eggs}")
+#---
+# #print(set_list[0])
+# # set_list = list(set_list)
+# # print(type(my_list),my_list,"\n",type(set_list),set_list)
+# # _str1 = "Напишите функцию"  #, которая принимает строку текста."
+# # def F4_2(data: str):
+# #     my_list = sorted(data,reverse=True)
+# #     set_list = {my_list}
+# #     for i in my_list:
+
+# #         pass
+# #     #res = 
+# #     return my_list
+# # print(F4_2(_str1))
