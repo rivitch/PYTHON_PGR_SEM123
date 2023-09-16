b220af9d3a4f96b19fcfc2dcfc35d2365313da64
«rivitch» <rivitch@rambler.ru>
2023-09-16 19:40:20 +0400

    deadline

--- Sem3/Sem3_Task3.py
+++ Sem3/Sem3_Task3.py
@@ -1,1 +1,11 @@
-items = {'спальник':5 палатка}
+from itertools import chain, combinations
+items = {'спальник': 5.0, 'палатка': 10.0, 'топор': 2.5, 'едаэ': 7.0, 'влда': 5.0,'радио': 3.0}
+capacity = int(input('Грузоподъёмность рюкзака'))
+backpack = 0
+pack =[]
+for name, weight in items.items():
+    if backpack + weight <= capacity:
+        backpack += weight
+        pack.append(name)
+pack.append(backpack)
+print(pack)
