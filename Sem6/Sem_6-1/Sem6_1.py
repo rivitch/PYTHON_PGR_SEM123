62eed0fc6ec5b9dd263b92ee4e7f7a2cde2072cd
«rivitch» <rivitch@rambler.ru>
2023-09-19 12:42:05 +0400

    Sem6_1b

---
+++ Sem6/Sem_6-1/Sem6_1.py
@@ -0,0 +1,10 @@
+import main_pokets
+
+from main_pokets import module_1 as bm
+from main_pokets.advanced_math import exp
+
+x = exp(2, 3)
+#print(main_pokets.module_1.bm.div(12, 5))
+print(bm.div(12, 5))
+
+print(f'{x}')
