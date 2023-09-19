8a2dfb961f56e3d2a4d5158036b10c11ae9c935c
«rivitch» <rivitch@rambler.ru>
2023-09-19 20:31:14 +0400

    Sem6_T1b

--- Sem6/Tasks/Sem6_Task1.py
+++ Sem6/Tasks/Sem6_Task1.py
@@ -1,8 +1,16 @@
-a,b,c,*_ = input("введите дату в формате DD.MM.YYYY: ").split('.')
 
-if 0<year<10000:
-    print("ошибка!")
-elif y%4 == 0 and ((y%100 != 0) or (y%400)) == 0:
-    print("год високосный!")
-else:
-    print("год не високосный!")
+
+
+
+day,month,year,*_ = input("введите дату в формате DD.MM.YYYY: ").split('.')
+
+def  bis_sextus(year):   # gh
+    print(int(year))
+    if 0<int(year)>10000:
+        print("ошибка!") 
+    elif int(year)%4 == 0 and int(year)%400 == 0:
+        print("год високосный!")
+    else:
+        print("год не високосный!")
+
+bis_sextus(year)
