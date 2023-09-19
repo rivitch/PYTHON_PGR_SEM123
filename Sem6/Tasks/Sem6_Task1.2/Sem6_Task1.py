b020089ce9293b2945bf7d47080612e5adae2fa9
«rivitch» <rivitch@rambler.ru>
2023-09-20 02:36:01 +0400

    Sem6_T1.2 made

---
+++ Sem6/Tasks/Sem6_Task1.2/Sem6_Task1.py
@@ -0,0 +1,25 @@
+
+
+
+from mods.test_data import f_test_data
+
+print(f'результат :{f_test_data()}\n')
+
+
+
+#-------------------------------------------------
+
+# #from mods.test import bis_sextus
+# from mods.test_data import f_test_data
+
+
+# print(f'результат :{f_test_data()}')
+# #day,month,year,*_ = input("введите дату в формате DD.MM.YYYY: ").split('.')
+
+
+# def t():
+#     pass        
+
+
+# #print(bis_sextus())
+# #print(bis_sextus(year))
--- Sem6/Tasks/Sem6_Task1.2/mods/test.py
+++ Sem6/Tasks/Sem6_Task1.2/mods/test.py
@@ -5,22 +5,15 @@
 def  bis_sextus(year) -> bool:   # проверка на високосный год
     #day,month,year,*_ = input("введите дату в формате DD.MM.YYYY: ").split('.')
     x = int(year)
-    print(f'{x} год н.э.')
+    #print(f'{x} год н.э.')
     if x<0 or x>10000: 
     #if int(year)<_START_YEAR or int(year)>_END_YEAR:   #_START_YEAR>int(year)>_END_YEAR:
         print("ошибка!") 
         return
     elif x%4 == 0 and x%400 == 0:
-        print("год високосный!")
+        #print("год високосный!")
         return True
     else:
-        print("год не високосный!")
+        #print("год не високосный!")
         return False
        
-
-# #_LIST_MONTH = [[1,31],[2,28],[3,31],[4,30],[5,31],[6,30],[7,31],[8,31],[9,30],[10,31],[11,30,[12,31]]]
-
-
-# def  bis_sextus(year) #-> bool:   # проверка на високосный год
-
-#     #return bool 
