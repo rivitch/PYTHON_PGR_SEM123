c010921d9f8cd7c0cdf2d8c5094e69a6dd1e08f5
«rivitch» <rivitch@rambler.ru>
2023-09-26 15:19:29 +0400

    повтор_а

--- Sem3/Sem3_4.py
+++ Sem3/Sem3_4.py
@@ -2,25 +2,26 @@
 # ✔ Создайте вручную список с повторяющимися элементами.
 # ✔ Удалите из него все элементы, которые встречаются дважды.
 
-# my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
-# out = []
-# print(my_list)
-# for i in my_list:
-#     # print(i, my_list.count(i))
-#     if my_list.count(i) != 2:
-#         # print(i, my_list)
-#         out.append(i)
-#         # print(out)
-# print(out)
+my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
+out = []
+print(my_list)
+for i in my_list:
+    # print(i, my_list.count(i))
+    if my_list.count(i) != 2:
+        # print(i, my_list)
+        out.append(i)
+        # print(out)
+print(out)
 
-# оставляем тех кто 2 раза
-# my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
-# out = []
-# print(my_list)
-# for i in my_list:
-#     if my_list.count(i) == 2:
-#         out.append(i)
-# print(out)
+#оставляем тех кто 2 раза
+my_list = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
+out = []
+print(my_list)
+for i in my_list:
+    if my_list.count(i) == 2:
+        out.append(i)
+out = set(out)        # лист в множество
+print(out)
 
 
         
--- Sem3/Sem3_Task1.py
+++ Sem3/Sem3_Task1.py
@@ -5,6 +5,8 @@
 _list1 = input("Введите список элементов, разделенных пробелом: ").split()
 out = []
 #_list1 = [2, 4, 6, 2, 8, 10, 10, 12, 2, 4, 14, 2]
+
+#out = (x = out.append(i) for i in _list1 if _list1.count(i) > 1)# AttributeError: 'generator' object has no attribute 'append'
 for i in _list1:
     if _list1.count(i) > 1:
         out.append(i)
