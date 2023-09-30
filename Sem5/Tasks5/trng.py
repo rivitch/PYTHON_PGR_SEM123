ac8390056ddf03fe136dcabda1921945ea5b471f
«rivitch» <rivitch@rambler.ru>
2023-09-30 09:18:53 +0400

    clear trng

--- Sem5/Tasks5/trng.py
+++
@@ -1,9 +0,0 @@
-txt = '1234567'
-res = [i for i in txt]
-print(res, type(res))
-res = {i for i in txt}
-print(res, type(res))
-res = (i for i in txt)
-print(res, type(res))
-print(*res, type(res))
-
