8cbe29c55a4a2c6169ae2597ffc5f818bfeb27a7
«rivitch» <rivitch@rambler.ru>
2023-10-11 00:18:58 +0400

    Task10-1a

--- Tasks10/main_factory.py
+++ Tasks10/main_factory.py
@@ -2,37 +2,67 @@
 # Класс принимает тип животного (название одного из созданных классов) и параметры для этого типа.
 # Внутри класса создайте экземпляр на основе переданного типа и верните его из класса-фабрики. 
 
+from random import randint
 
-class Animal:
-    def __init__(self, name, age):  #, spec = None
-        self.name = name
-        self.age = age
-        #self.spec = spec
-    def get_spec(self):
-        return self.spec
-    pass
+list_animal = []
 
-class Dog(Animal):
-    def __init__(self, name, age, spec):
-        super().__init__(name, age)
-        self.spec = spec
+class AnimalFactory:
+    def create_animal(self, animal_type, **kwargs):
+        if animal_type == "Fish":
+            return Fish(**kwargs)
+        elif animal_type == "Tiger":
+            return Tiger(**kwargs)
+        elif animal_type == "Eagle":
+            return Eagle(**kwargs)
+        else:
+            raise ValueError(f"Unsupported animal type: {animal_type}")
 
-class Cat(Animal):
-    def __init__(self, name, age, spec):
-        super().__init__(name, age)
-        self.spec = spec
+class Fish:
+    def __init__(self, **kwargs):  # инициализация для класса Fish
+        pass
 
-class Fish(Animal):
-    def __init__(self, name, age, spec):
-        super().__init__(name, age)
-        self.spec = spec
+class Tiger:
+    def __init__(self, **kwargs):  # инициализация для класса Tiger
+        pass
 
-class Factory(Animal):
-    def __init__(self, name):#, age, spec
-        # super().__init__(name)
-        self.spec = spec
+class Eagle:
+    def __init__(self, **kwargs):  # инициализация для класса Eagle
+        pass
 
+factory = AnimalFactory()
+animal = factory.create_animal("Cat", name="Whiskers", age=3)
 
-name, age, spec, *_ = input().split()
-a = Factory(Fish(name, age, spec))
-print(a.get_spec())
+#==========================
+# class Animal:
+#     def __init__(self, name, age):  #, spec = None
+#         self.name = name
+#         self.age = age
+#         #self.spec = spec
+#     def get_spec(self):
+#         return self.spec #self.name, self.age, 
+#     pass
+
+# class Dog(Animal):
+#     def __init__(self, name, age, spec):
+#         super().__init__(name, age)
+#         self.spec = spec
+
+# class Cat(Animal):
+#     def __init__(self, name, age, spec):
+#         super().__init__(name, age)
+#         self.spec = spec
+
+# class Fish(Animal):
+#     def __init__(self, name, age, spec):
+#         super().__init__(name, age)
+#         self.spec = spec
+
+# class Factory(Animal):
+#     def __init__(self, name):#, age, spec
+#         # super().__init__(name)
+#         self.spec = spec
+    
+
+# name, age, spec, *_ = input().split()
+# a = Factory(Dog(name, age, spec))
+# print(a.get_spec())
